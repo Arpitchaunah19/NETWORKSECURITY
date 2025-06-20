815b8ae2bd2bd49053f62ad0202e8af51817846e
unknown <arpitchaunah09@gmail.com>
2025-06-20 17:14:04 +0530

    data ingestion component

--- text_mongodb.py
+++ text_mongodb.py
@@ -1,11 +1,10 @@
-
+import certifi
 from pymongo.mongo_client import MongoClient
 
 uri = "mongodb+srv://geek08707:<@password>@cluster0.dvexmne.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
-client = MongoClient(uri)
-
+client =MongoClient(uri)
 # Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
