80d597158fa77bc4f58db244fc7ef9ed8468667a
unknown <arpitchaunah09@gmail.com>
2025-06-23 15:29:59 +0530

    transformation

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -27,8 +27,8 @@ DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2 
 
 """
-Data Valadation related constant start with Data_valadation var name
 
+Data Valadation related constant start with Data_valadation var name
 """
 DATA_VALIDATION_DIR_NAME: str = "data_validation"
 DATA_VALIDATION_VALID_DIR: str = "Validated"
@@ -37,3 +37,17 @@ DATA_VALIDATION_REPORT_DIR: str = "drift_report"
 DATA_VALIDATION_REPORT_FILE_NAME: str = "report.yaml"
 DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
 DATA_VALIDATION_REPORT_FILE_NAME: str = "drift_report.json"
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+
+
+
+
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+
+DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
+    "missing_values": np.nan,
+    "n_neighbors": 3,
+    "weights": "uniform",
+}
--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -51,7 +51,7 @@ class DataValidationConfig:
         )
 
 
-'''class DataTransformationConfig:
+class DataTransformationConfig:
      def __init__(self,training_pipeline_config:TrainingPipelineConfig):
         self.data_transformation_dir: str = os.path.join( training_pipeline_config.artifact_dir,training_pipeline.DATA_TRANSFORMATION_DIR_NAME )
         self.transformed_train_file_path: str = os.path.join( self.data_transformation_dir,training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
@@ -71,4 +71,4 @@ class ModelTrainerConfig:
             training_pipeline.MODEL_FILE_NAME
         )
         self.expected_accuracy: float = training_pipeline.MODEL_TRAINER_EXPECTED_SCORE
-        self.overfitting_underfitting_threshold = training_pipeline.MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD'''
+        self.overfitting_underfitting_threshold = training_pipeline.MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD
